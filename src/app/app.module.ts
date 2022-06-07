98fe0df4c747b312362a8d04a399c67a0c384cd5
Francisco Javier Gil Bautista <avexyk@gmail.com>
2022-06-07 11:35:15 -0500

    add: template sin rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,

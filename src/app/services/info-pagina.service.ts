34863059774ca94876acf054e881d20498738d90
Francisco Javier Gil Bautista <avexyk@gmail.com>
2022-06-18 19:11:45 -0500

    bd en firebase, servicio para obtener datos y mostrar datos en el componente

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { equipoPagina } from '../interfaces/equipo-pagina.interface';
 import { infoPagina } from '../interfaces/info-pagina.interface';
 
 @Injectable({
@@ -9,27 +10,53 @@ export class InfoPaginaService {
 
   info: infoPagina = {};
   cargada: boolean = false;
+  equipo: any = {};
 
   constructor(
     private http: HttpClient
   ) { 
       // console.log( 'Servicio cargado' );
 
-      this.http.get('assets/data/data-pagina.json')
-          .subscribe( (resp: infoPagina) => {
+      // this.http.get('assets/data/data-pagina.json')
+      //     .subscribe( (resp: infoPagina) => {
 
-            // console.log( resp );
+      //       // console.log( resp );
 
-            // type ObjectKey = keyof typeof resp;
-            // const email = 'email' as ObjectKey;
+      //       // type ObjectKey = keyof typeof resp;
+      //       // const email = 'email' as ObjectKey;
 
-            this.cargada = true;
-            this.info = resp;
+      //       this.cargada = true;
+      //       this.info = resp;
 
-            console.log( resp );
+      //       console.log( resp );
 
-            // console.log( resp[email] );
+      //       // console.log( resp[email] );
 
+      //     });
+
+      this.cargarInfo();
+      this.cargarEquipo();
+
+   }
+
+   private cargarInfo() {
+    //  Leer JSON
+     this.http.get('assets/data/data-pagina.json')
+         .subscribe( (resp: infoPagina) => {
+           this.cargada = true,
+           this.info = resp;
+
+           console.log( this.info );
+         });
+   }
+
+   private cargarEquipo() {
+      this.http.get('https://historie-c87e5-default-rtdb.firebaseio.com/equipo.json')
+          .subscribe( (resp: equipoPagina) => {
+            this.cargada = true,
+            this.equipo = resp;
+            
+            console.log( this.equipo );
           });
    }
 }
